d551852f6690f39d6599310b7370667c2efbe18f
sandeshbodake <sandeshbodake121@gmail.com>
2024-03-24 17:07:47 +0530

    fix: eslint

--- app/javascript/components/commons/PrivateRoute.jsx
+++ app/javascript/components/commons/PrivateRoute.jsx
@@ -1,35 +1,35 @@
 import React from "react";
 
 import PropTypes from "prop-types";
-import {Navigate, Route} from "react-router-dom";
+import { Navigate, Route } from "react-router-dom";
 
 const PrivateRoute = ({
-                          component: Component,
-                          condition,
-                          path,
-                          redirectRoute,
-                          ...props
-                      }) => {
-    if (!condition) {
-        return (
-            <Navigate
-                to={{
-                    pathname: redirectRoute,
-                    from: props.location,
-                }}
-            />
-        );
-    }
+  component: Component,
+  condition,
+  path,
+  redirectRoute,
+  ...props
+}) => {
+  if (!condition) {
+    return (
+      <Navigate
+        to={{
+          pathname: redirectRoute,
+          from: props.location,
+        }}
+      />
+    );
+  }
 
-    return <Route component={Component} path={path} {...props} />;
+  return <Route component={Component} path={path} {...props} />;
 };
 
 PrivateRoute.propTypes = {
-    component: PropTypes.func,
-    condition: PropTypes.bool,
-    location: PropTypes.object,
-    path: PropTypes.string,
-    redirectRoute: PropTypes.string,
+  component: PropTypes.func,
+  condition: PropTypes.bool,
+  location: PropTypes.object,
+  path: PropTypes.string,
+  redirectRoute: PropTypes.string,
 };
 
 export default PrivateRoute;
