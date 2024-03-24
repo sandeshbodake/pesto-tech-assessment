e9426a38938a77a04cb1db78b1beabbb774f93cb
sandeshbodake <sandeshbodake121@gmail.com>
2024-03-24 16:33:02 +0530

    fix: eslint setup

---
+++ .eslint-rules/overrides.js
@@ -0,0 +1,25 @@
+module.exports = {
+  // Currently we are using this section for excluding certain files from certain rules.
+  overrides: [
+    {
+      files: [
+        ".eslintrc.js",
+        ".prettierrc.js",
+        "app/assets/**/*",
+        "app/javascript/packs/**/*",
+        "*.json",
+      ],
+      rules: {
+        "import/order": "off",
+        "react-hooks/rules-of-hooks": "off",
+        "react/jsx-filename-extension": "off",
+      },
+    },
+    {
+      files: ["app/javascript/packs/**/*.{js,jsx}"],
+      rules: {
+        "no-redeclare": "off",
+      },
+    },
+  ],
+};
